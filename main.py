29efe7ad730dd82df6ca0b804aa6de0f0de2aa19
Ikbol <muxtorovikboljon1@gmail.com>
2024-03-01 20:43:47 +0500

    1 chi masalani ishladm

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 
+'''
+1 chi masala
+'''
+
+# from collections import namedtuple
+#
+# Person = namedtuple('Person', 'id, name, region, price, interes')
+#
+#
+# books = [
+#     (1, 'Shaytanat', 'Uzbekistan', 100000, 'Jangari'),
+#     (2, 'Qizil_Atirgul', 'Qazogiston', 20000, 'Muhabbat'),
+#     (3, 'Alpomish', 'Qirgiziston', 50000, 'Jangari'),
+#     (4, 'Toxir_va_Zuxra', 'Misr', 3000, 'Sevgi va Muhabbat')
+# ]
+#
+# for i in books:
+#     user = Person(*i)
+#     print(user)
 
 
-
+#=========================================================================================================
 
 
 
